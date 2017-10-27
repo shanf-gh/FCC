060951649930b423c68308052c79dd9458c2fede
Fabien SHAN <shan.fabien@gmail.com>
2017-10-27 16:55:24 +0900

    Sucessfully got data from JSON

--- 3. Local Weather/assets/script.js
+++ 3. Local Weather/assets/script.js
@@ -1,7 +1,9 @@
 var x = document.getElementById("data");
 var y = document.getElementById("message");
 var z = document.getElementById("variable");
+var data;
 
+// Function to get the current location's information
 function getLocation () {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -10,34 +12,48 @@ function getLocation () {
   }
 }
 
+
 function showPosition(position) {
+  // get lattitude and longitude
   var lat = position.coords.latitude;
   var lon = position.coords.longitude;
+  // output lattiude and longitude to html
   x.innerHTML = "Latitude: " + Math.round(position.coords.latitude) +
   "<br>Longitude: " + Math.round(position.coords.longitude);
 
+  // Create a new request
   var request = new XMLHttpRequest();
-    var getURL = 'https://fcc-weather-api.glitch.me/api/current?lat='+ lat +'&lon=' + lon;
-    request.open('GET', getURL, true);
+  var getURL = 'https://fcc-weather-api.glitch.me/api/current?lat='+ lat +'&lon=' + lon;
+  request.open('GET', getURL, true);
 
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400) {
-        // SUCCESS !
-        var data = request.responseText;
-        y.innerHTML = data;
-        z.innerHTML = 'lat: ' + lat + '<br>lon: ' + lon;
-      } else {
-        // We reached our target server, but it returned an error
-        y.innerHTML = "Server reached. Error returned";
-      }
-    };
+  //
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      // SUCCESS !
+      // Stringify the response from the server and then parse to create a JS object
+      data = JSON.parse(request.responseText);
+      y.innerHTML = data.weather[0].main;
+      z.innerHTML = 'lat: ' + lat + '<br>lon: ' + lon;
+      document.getElementById("loc_lon-lat").innerHTML = "longitude: " + data.coord.lon + "; lattitude: " + data.coord.lat;
+      document.getElementById("loc").innerHTML = "Data name: " + data.name;
+      document.getElementById("temp").innerHTML = "temperature: " + data.main.temp;
+      document.getElementById("weather-main").innerHTML = "Weather param: " + data.weather[0].main;
+      document.getElementById("weather-icon").innerHTML = "Weather icon: <img src=\"" + data.weather[0].icon + "\">";
+      /**/
+    } else {
+      // We reached our target server, but it returned an error
+      y.innerHTML = "Server reached. Error returned";
+    }
+  };
 
-    request.onerror = function() {
-      // There was a connection error of some sort
-      y.innerHTML = "Connection error";
-    };
+  // If request returns an error
+  request.onerror = function() {
+    // There was a connection error of some sort
+    y.innerHTML = "Connection error";
+  };
 
-    request.send();
+  // send the request
+  request.send();
 
 }
 
