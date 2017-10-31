f87da5da745daaca47c96704e43060f275de11df
Fabien SHAN <shan.fabien@gmail.com>
2017-10-31 14:09:06 +0900

    Completed Local Weather project - MED version

--- 3. Local Weather/assets/script.js
+++ 3. Local Weather/assets/script.js
@@ -1,52 +1,68 @@
 
 // divs to be populated
-// If uncomment verify all related element
-// var lon_lat = document.getElementById("loc_lon-lat");
 var loc = document.getElementById("loc");
-var temp = document.getElementById("temp");
+var tempV = document.getElementById("tempV");
+var tempM = document.getElementById("tempM");
 var weather = document.getElementById("weather-main");
 var weather_icon = document.getElementById("weather-icon");
-var y = document.getElementById("message");
-var z = document.getElementById("variable");
 var data;
 
-// Function to get the current location's information
-function getLocation () {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    y.innerHTML = "Geolocalisation is not supported by this browser.";
-  }
+// Geolocation on page load
+window.onload = function() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      y.innerHTML = "Geolocalisation is not supported by this browser.";
+    }
 }
 
+// Change temperature when clicking on the temperature's measure
+document.getElementById("tempM").onclick = function() {
+  // Get temperature value and measure
+  var tempVal = tempV.innerText.substring(0, tempV.innerText.length-2);
+  var tempMea = tempM.innerText;
 
+  // Apply formula and measure change based on current measure
+  if(tempMea === "C") {
+    tempVal = tempVal * 9 / 5 + 32;
+    tempV.innerHTML = Math.round(tempVal * 10) / 10 + " &#176;";
+    tempM.innerHTML = "F";
+  } else if (tempMea === "F") {
+    tempVal = (tempVal - 32) * 5 / 9;
+    tempV.innerHTML = Math.round(tempVal * 10) / 10 + " &#176;";
+    tempM.innerHTML = "C";
+  }
+};
+
+// Show position
 function showPosition(position) {
   // get lattitude and longitude
   var lat = position.coords.latitude;
   var lon = position.coords.longitude;
-  // output lattiude and longitude to html
   // Create a new request
   var request = new XMLHttpRequest();
+  // Build URL for request - need latttitude and longitude
   var getURL = 'https://fcc-weather-api.glitch.me/api/current?lat='+ lat +'&lon=' + lon;
+  // GET request
   request.open('GET', getURL, true);
 
-  //
+  // make the request and get data
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
       // SUCCESS !
-      // Stringify the response from the server and then parse to create a JS object
+      // get the responsetext and parse it to turn it into a JS object
       var reqResponse = request.responseText;
       data = JSON.parse(reqResponse);
-      y.innerHTML = reqResponse;
-      // if uncomment - uncomment related html div
-      // lon_lat.innerHTML = "lon: " + data.coord.lon + " lat: " + data.coord.lat;
+      console.log(data);
+      // var icon = "http://openweathermap.org/img/w/"+ data.weather[1].icon +".png";
+      var icon = data.weather[0].icon;
       loc.innerHTML = data.name + ", " + data.sys.country;
-      // Character for Celcius &#8451; - for fahrenheit &#8457;
-      temp.innerHTML =  Math.round(data.main.temp * 10) / 10 + " &#8451;";
-
+      // Character for degree &#176;
+      tempV.innerHTML =  Math.round(data.main.temp * 10) / 10 + " &#176;";
+      tempM.innerHTML = "C";
       weather.innerHTML = data.weather[0].main;
-      weather_icon.innerHTML = "<img src=\"" + data.weather[0].icon + "\">";
-      /**/
+      weather_icon.innerHTML = "<img src=\"" + icon + "\">";
+
     } else {
       // We reached our target server, but it returned an error
       y.innerHTML = "Server reached. Error returned";
@@ -61,17 +77,4 @@ function showPosition(position) {
 
   // send the request
   request.send();
-
 }
-
-/*
-// Runs only if the HTML is fully loaaded
-$(document).ready(function(){
-  // click event handled on .curLoc
-  $(".curLoc").on("click", function(){
-    // Modify the html in the elements with .message class
-    $(".message").html("Here is the message");
-
-  });
-});
-*/
