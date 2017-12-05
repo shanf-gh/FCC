9b9cfcfa50217982a5f1ec9973d6961a6ba6e98b
Fabien SHAN <shan.fabien@gmail.com>
2017-12-05 12:15:02 +0900

    Online/Offline response and CSS style

--- 5. Twitchtv/assets/script.js
+++ 5. Twitchtv/assets/script.js
@@ -1,56 +1,118 @@
 
 
 window.onload = function() {
-  // Document elements
+  // document elements
   var results = document.getElementById("results");
 
   // users array
-  var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
+  var users = ["test_channel","ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
               "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","dahmien7"]
 
-  var endpoint = "https://wind-bow.glitch.me/twitch-api/streams/";
 
-  // set an iterator to 0, define a function that make an xhr request and take a callback ,
-  // call that function, check the iterator against your array length,
-  // inside the callback increment the iterator and call the function again
+  // define endpoints
+  var endpoint_s = "https://wind-bow.glitch.me/twitch-api/streams/";
+  var endpoint_c = "https://wind-bow.glitch.me/twitch-api/channels/";
 
-  for (var i = 0, n = users.length; i < n; i++) {
-    var url = endpoint + users[i];
+
+
+  function doAjax() {
+    for (var i = 0, n = users.length; i < n; i++) {
+      getData('stream',users[i]);
+    }
+  }
+
+  function processData(data, user) {
+    /* stream status */
+    if (data.stream === null) {
+      /* Stream is offline */
+      getData('channel', user);
+    } else {
+      if (!data.hasOwnProperty('logo')) {
+        data = data.stream.channel;
+        var stat_sign = '&#10004;';
+        var stat_color = 'green';
+        var line_status = 'online';
+      } else {
+        var stat_sign = '&#10006;';
+        var stat_color = 'red';
+        var line_status = 'offline';
+      }
+
+      /* icon */
+      var logo = document.createElement('img');
+      logo.src = data.logo;
+      var icon = document.createElement('div');
+      icon.className = 'icon';
+      icon.appendChild(logo);
+
+      /* name & status */
+      var name = document.createElement('p');
+      name.innerHTML = data.display_name;
+      var c_status = document.createElement('p');
+      c_status.className = 'c_status';
+      c_status.innerHTML = data.status;
+      var name_status = document.createElement('div');
+      name_status.appendChild(name);
+      name_status.appendChild(c_status);
+
+      /* Online/Offline */
+      var s_status = document.createElement('div');
+      s_status.className = 's_status';
+      s_status.style.color = stat_color;
+      s_status.innerHTML = stat_sign;
+
+      /* appending to results */
+      var row = document.createElement('div');
+      row.className = 'row ' + line_status;
+      row.appendChild(icon);
+      row.appendChild(name_status);
+      row.appendChild(s_status);
+      results.appendChild(row);
+    }
+  }
+
+  function getData(type, user) {
+    if (type === 'channel') {
+      var url = endpoint_c + user;
+    } else if (type === 'stream') {
+      var url = endpoint_s + user;
+    }
 
     var xhr = new XMLHttpRequest();
-
     xhr.open('GET', url, true);
-
     xhr.onload = function() {
       if (this.status >= 200 && this.status < 400) {
-        // Success!
-        var data = JSON.parse(this.response);
-        if (data.stream) {
-          console.log("Offline");
-        } else {
-          console.log("Online");
-
-          var img = document.createElement("img");
-          img.src = data.stream.logo;
-          results.appendChild(img);
-        }
-        console.dir(data.stream);
-        console.dir(data);
+        data = JSON.parse(this.responseText);
+        processData(data,user);
       } else {
-        // We reached our target server, but it returned an error
-        results.innerHTML =  "<p>Server reached. Error returned</p>";
+        console.log("Server reached. Error returned for " + user);
       }
-    };
-
+    }
     xhr.onerror = function() {
       // There was a connection error of some sort
-      results.innerHTML = "<p>Connection error</p>";
+      // Connection error
+      console.log("Connection error for " + users[i]);
     };
-
     xhr.send();
   }
 
+  doAjax();
 }
+
+/* Online filter */
+/* Disable link's default behavior */
+var menu_links = document.getElementById('status').getElementsByTagName('a');
+
+for (var i = 0, n = menu_links.length; i < n; i++) {
+  menu_links[i].addEventListener('click', function(e){e.preventDefault();});
+}
+
+// select elements: document.querySelectorAll(".example")
+// visibility of element: document.getElementById("myP").style.visibility = "hidden";
+var stream_on = results.getElementsByClassName('online');
+var stream_off = results.getElementsByClassName('offline');
+
+
 
 // Have a look for asynchronous requests:
 // https://stackoverflow.com/questions/5485495/how-can-i-take-advantage-of-callback-functions-for-asynchronous-xmlhttprequest
