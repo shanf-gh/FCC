a2e0132711282aa959158f89f8c23d73fae40d48
Fabien SHAN <shan.fabien@gmail.com>
2018-01-27 16:58:57 +0900

    Finished JS calculator

--- 6. JS Calculator/assets/script.js
+++ 6. JS Calculator/assets/script.js
@@ -1,40 +1,48 @@
 var result = 0;
 var newNum = 0;
+var memory = 0;
 var nextOperation = "";
 var screenContent = document.getElementById("screen-content");
 
+// Logics from: http://www.calculator.org/calchelp/basics.aspx
+
 function handleclick(val) {
-    console.log(val);
-    console.log("Operation: "+ nextOperation + "; result:" + result + "; newNum:" + newNum);
           
     switch(true) {
         case val === "ON/C":
-            result = 0;
-            newNum = 0;
+            resetVar();
+            display(result);
             screenContent.style.display = screenContent.style.display === 'none' ? 'block' : 'none';
             break;
-        case (val >= 0 && val <= 9):
-            if (newNum.length > 1) {
+        case val === "MRC": // Displays value in memory
+            display(memory);
+            break;
+        case val === "M-": // Delete displayed value from memory
+            memory -= result;
+            resetVar();
+            break;
+        case val === "M+": // Add displayed value to memory
+            memory += result;
+            resetVar();
+            break;
+        case (val >= 0 && val <= 9): // Handle digits input
+            if (newNum !== 0) {
                 newNum += val;
             } else {
                 newNum = parseFloat(val);
             }
             display(newNum);
             break;
-        case val === ".":
+        case val === ".": // Handle decimal point and check for existing point
             if (!newNum.toString().includes(".")) {
                 newNum += ".";
             }
+            display(newNum);
             break;
-        case val === "C":
-            result = 0;
-            newNum = 0;
-            display(result);
-            break;
-        case val === "√": // "&#8730;"
-            result = Math.sqrt(newNum);
-            display(result);
-            newNum = 0;
+        case val === "=": // Compute and return result
+            newNum = parseFloat(newNum);
+            performCalc();
+            nextOperation = "";
             break;
         case val === "+":
         case val === "-": // "&#45;"
@@ -47,10 +55,36 @@ function handleclick(val) {
             if (result === 0) result += parseFloat(newNum);
             newNum = 0;
             break;
-        case val === "=":
-            newNum = parseFloat(newNum);
-            performCalc();
+        case val === "√": // "&#8730;"
+            result = Math.sqrt(newNum);
+            display(result);
+            newNum = 0;
+            break;
+        case val === "%":
+            switch(nextOperation) {
+                case "+":
+                    result = result * (1 + newNum/100);
+                    break;
+                case "-": // "&#45;"
+                    result = result * (1 - newNum/100);
+                    break;
+                case "×": // "&#215;"
+                    result = result * newNum/100;
+                    break;
+                case "÷" : // "&#247;"
+                    result = result / newNum * 100;
+                    break;
+            }
+            display(result);
+            break;
+        case val === "C":
+            resetVar();
+            display(result);
             nextOperation = "";
+            break;
+        case val === "CE": // Clear last entry
+            newNum = 0;
+            display(newNum);
             break;
     } 
 }
@@ -78,4 +112,10 @@ function performCalc() {
     }
     newNum = 0;
     display(result);
+}
+
+// Reset result and newNum
+function resetVar() {
+    result = 0;
+    newNum = 0;
 }
