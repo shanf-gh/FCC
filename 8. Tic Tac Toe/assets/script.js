346133e36d83229a76f790676c191fbe0532e322
Fabien SHAN <shan.fabien@gmail.com>
2018-02-07 18:18:48 +0900

    Setting up initial state for testing minmax implementation. Set up display board, had problems with the array not being correct. Started with a dynamic implementation but it really does not matter so will have a hardcoded array instead.

--- 8. Tic Tac Toe/assets/script.js
+++ 8. Tic Tac Toe/assets/script.js
@@ -1,11 +1,24 @@
+// =======================================================
+//                      Initialize
+// =======================================================
+
 var playerStart = true; // true: player plays first
 
+// fontawsome icons
 var cross = '<i class="fas fa-times fa-6x">';
 var circle = '<i class="far fa-circle fa-5x">';
 
 var turnCross = true; // true: next click will show cross
 var gameOver = false;
+
 var boardStatus = new Array(9).fill(null);
+var squares = document.getElementsByClassName('square');
+
+// squares.forEach(function(el, i){
+//     square[i] = el.firstChild.classList[1];
+// });
+
+console.log(squares);
 
 // =======================================================
 //                      Logics
@@ -14,18 +27,18 @@ function handleClick(element) {
     var id = element.id;
     var square = document.getElementById(id);
 
-    // if square is not empty -> error
-    if (square.innerHTML !== "" && gameOver) {
+    // if square is not empty -> error or if gameOver
+    if (square.innerHTML !== "" || gameOver) {
         // console.log('This square is not empty!');
         return 0;
     }
     // show cross or circle based on turnCross boolean
     square.innerHTML = turnCross ? cross : circle;
-    boardStatus[id] = turnCross ? 'x' : 'o';
+    boardStatus[id - 1] = turnCross ? 'x' : 'o';
     // toggle for next turn
     turnCross = !turnCross;
-
-    console.log(checkBoard());
+    // check if won
+    checkBoard();
 }
 
 function restart() {
@@ -34,31 +47,29 @@ function restart() {
 
 function checkBoard() {
     var winTable = [
-        [1,2,3], [4,5,6], [7,8,9], // rows
-        [1,4,7], [2,5,8], [3,6,9], // columns
-        [1,5,9], [3,5,7]           // diagonals
+        [0,1,2], [3,4,5], [6,7,8], // rows
+        [0,3,6], [1,4,7], [2,5,8], // columns
+        [0,4,8], [2,4,6]           // diagonals
     ];
 
     winTable.forEach(function(combination) {
         if(boardStatus[combination[0]] !== null && 
             boardStatus[combination[0]] === boardStatus[combination[1]] &&
             boardStatus[combination[0]] === boardStatus[combination[2]] ) {
-                console.log(boardStatus);
-                console.log(boardStatus[combination[0]]);
              // winning combination
-             return 1;
+             gameOver = true;
+             return gameOver;
          }
     })
-
-    // for (var i = 0, n = winTable.length; i < n; i++) {
-    //     var combination = winTable[i];
-    //     if(boardStatus[combination[0]] !== null && 
-    //        boardStatus[combination[0]] === boardStatus[combination[1]] &&
-    //        boardStatus[combination[0]] === boardStatus[combination[2]] ) {
-    //         // winning combination
-    //         return 1;
-    //     }
-    // }
-    // No winning combination
     return 0;
+}
+
+// Computer turn
+
+function computer() {
+    
+    
+
+    // toggle for next turn
+    turnCross = !turnCross;
 }
