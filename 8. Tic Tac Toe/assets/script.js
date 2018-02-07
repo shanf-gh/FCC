f353dfbce3dfdedd2b2d7eb82d726087141affd8
Fabien SHAN <shan.fabien@gmail.com>
2018-02-07 14:49:35 +0900

    Board structure is Table with 3 table rows containing 3 td elements. Gave different id to each td and added onclick handling event. Coded handleClick to show cross or circle icon from fontawesome based on turn (turnCross boolean).

--- 8. Tic Tac Toe/assets/script.js
+++ 8. Tic Tac Toe/assets/script.js
@@ -1,2 +1,22 @@
-console.log('Script loaded!');
+var playerStart = true; // true: player plays first
+
+var cross = '<i class="fas fa-times fa-6x">';
+var circle = '<i class="far fa-circle fa-5x">';
+
+var turnCross = true; // true: next click will show cross
+
+function handleClick(element) {
+    var id = element.id;
+    var square = document.getElementById(id);
+
+    // if square is not empty -> error
+    if (square.innerHTML !== "") {
+        // console.log('This square is not empty!');
+        return 0;
+    }
+    // show cross or circle based on turnCross boolean
+    square.innerHTML = turnCross ? cross : circle;
+    
+    turnCross = !turnCross;
+}
 
