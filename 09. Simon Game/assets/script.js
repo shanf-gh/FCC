33c9ab074ab337665ac0c8527e7fc0ca3ecd4077
Fabien SHAN <shan.fabien@gmail.com>
2018-02-14 23:32:18 +0900

    Board outline, button styling. Play audio onclick

--- 09. Simon Game/assets/script.js
+++ 09. Simon Game/assets/script.js
@@ -1,1 +1,14 @@
 console.log('Script is connected!');
+
+
+
+
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${this.dataset.key}"]`);
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const buttons = Array.from(document.querySelectorAll('.game-button'));
+buttons.forEach(button => button.addEventListener('click',playSound));
