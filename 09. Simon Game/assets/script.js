0d4de64d25daa03228ac2c1da2f9b0856d553766
Fabien SHAN <shan.fabien@gmail.com>
2018-02-16 15:24:15 +0900

    Control Board design

--- 09. Simon Game/assets/script.js
+++ 09. Simon Game/assets/script.js
@@ -1,8 +1,22 @@
-console.log('Script is connected!');
+// ========================
+//        Controls
+// ========================
+
+function toggleButton() {
+    if(this.style.marginLeft === "40%") {
+        this.style.marginLeft = 0;
+    } else {
+        this.style.marginLeft = "40%";
+    }
+}
+
+const powerToggle = document.getElementsByClassName('power-slide__toggle')[0];
+powerToggle.addEventListener('click', toggleButton)
 
 
-
-
+// ========================
+//        Game logics
+// ========================
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${this.dataset.key}"]`);
