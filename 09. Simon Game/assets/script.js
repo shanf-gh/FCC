4b717487fc98ed5b1531c926892c7ddf58794167
Fabien SHAN <shan.fabien@gmail.com>
2018-02-18 21:55:26 +0900

    Continuing design of control board. CSS Grid for styling and toggling of control buttons

--- 09. Simon Game/assets/script.js
+++ 09. Simon Game/assets/script.js
@@ -10,9 +10,26 @@ function toggleButton() {
     }
 }
 
-const powerToggle = document.getElementsByClassName('power-slide__toggle')[0];
-powerToggle.addEventListener('click', toggleButton)
+const powerToggle = Array.from(document.getElementsByClassName('power-slide__toggle'));
+powerToggle.forEach(toggle => toggle.addEventListener('click', toggleButton));
 
+function toggleButton2() {
+    const curVal = parseInt(this.style.gridColumnStart);
+    const nrPosition = this.dataset.position;
+    var newVal = 0;
+
+    if(!curVal) {
+        newVal = 2;
+    } else if (curVal < nrPosition) {
+        newVal = curVal + 1;
+    } else {
+        newVal = 1;
+    };
+    this.style.gridColumnStart = newVal;
+}
+
+const powerToggle2 = Array.from(document.getElementsByClassName('slide__toggle'));
+powerToggle2.forEach(toggle => toggle.addEventListener('click', toggleButton2));
 
 // ========================
 //        Game logics
