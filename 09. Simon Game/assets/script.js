26a45d728207bad1bc99ce64e0e3f988637775a4
Fabien SHAN <shan.fabien@gmail.com>
2018-02-19 16:52:08 +0900

    Starting working on logics

--- 09. Simon Game/assets/script.js
+++ 09. Simon Game/assets/script.js
@@ -3,18 +3,7 @@
 // ========================
 
 function toggleButton() {
-    if(this.style.marginLeft === "20%") {
-        this.style.marginLeft = 0;
-    } else {
-        this.style.marginLeft = "20%";
-    }
-}
-
-const powerToggle = Array.from(document.getElementsByClassName('power-slide__toggle'));
-powerToggle.forEach(toggle => toggle.addEventListener('click', toggleButton));
-
-function toggleButton2() {
-    const curVal = parseInt(this.style.gridColumnStart);
+    const curVal = parseInt(window.getComputedStyle(this).getPropertyValue('grid-column-start'));
     const nrPosition = this.dataset.position;
     var newVal = 0;
 
@@ -28,12 +17,14 @@ function toggleButton2() {
     this.style.gridColumnStart = newVal;
 }
 
-const powerToggle2 = Array.from(document.getElementsByClassName('slide__toggle'));
-powerToggle2.forEach(toggle => toggle.addEventListener('click', toggleButton2));
+const powerToggle = Array.from(document.getElementsByClassName('slide__toggle'));
+powerToggle.forEach(toggle => toggle.addEventListener('click', toggleButton));
 
 // ========================
 //        Game logics
 // ========================
+
+// Playing sounds
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${this.dataset.key}"]`);
@@ -43,3 +34,52 @@ function playSound(e) {
 
 const buttons = Array.from(document.querySelectorAll('.game-button'));
 buttons.forEach(button => button.addEventListener('click',playSound));
+
+// Registering player's game
+// playable buttons
+const buttonsKey = ['green', 'red', 'blue', 'yellow']
+
+// Computer sequence
+var computerSequence = [];
+
+function startGame() {
+    const min = 0;
+    const max = 3;
+    var random = Math.floor(Math.random() * (max - min) + 1) + min;
+    var playKey = buttonsKey[random];
+    
+    if(computerSequence.length>0) {
+        for (let i = 0; i < computerSequence.length; i++) {
+            playButton(computerSequence[i]);    
+        }
+
+        // computerSequence.forEach(key => playButton(key));
+    }
+    console.log("played new!");
+    playButton(playKey);  
+    computerSequence.push(playKey);   
+    console.log(computerSequence);
+}
+
+function playButton(button) {
+    const audio = document.querySelector(`audio[data-key="${button}"]`);
+    audio.play();
+}
+
+document.getElementsByClassName("js-start")[0].addEventListener('click', startGame);
+
+// Array to register the player
+var player = [];
+
+// listening function
+function playSound2(e) {
+    const audio = document.querySelector(`audio[data-key="${this.dataset.key}"]`);
+    const button = this;
+    audio.currentTime = 0;
+    audio.play();
+    player.push(button.dataset.key);
+
+    button.classList.toggle("active")
+    setTimeout(() => button.classList.toggle("active"),500);
+}
+buttons.forEach(button => button.addEventListener('click',playSound2));
