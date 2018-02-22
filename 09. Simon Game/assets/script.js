3a98102657abcd71e1df46c61ce3179cedd14894
Fabien SHAN <shan.fabien@gmail.com>
2018-02-22 23:24:38 +0900

    Working on control board logics

--- 09. Simon Game/assets/script.js
+++ 09. Simon Game/assets/script.js
@@ -1,28 +1,45 @@
 // ========================
 //        Controls
 // ========================
+const buttonToggle = Array.from(document.getElementsByClassName('slide__toggle'));
+var environmentVar = {
+    gameStart: false,
+};
+
+buttonToggle.forEach(toggle => { 
+    const name = toggle.dataset.btn;
+    const currVal = parseInt(window.getComputedStyle(toggle).getPropertyValue('grid-column-start'));
+    environmentVar[name] = currVal;
+});
 
 function toggleButton() {
     const curVal = parseInt(window.getComputedStyle(this).getPropertyValue('grid-column-start'));
     const nrPosition = this.dataset.position;
-    var newVal = 0;
+    const btnName = this.dataset.btn;
+    if (environmentVar.power === 2 && this.dataset.btn !== 'power') return; // power === 1 is off, 2 is on
 
+    var newVal = 1;
     if(!curVal) {
         newVal = 2;
     } else if (curVal < nrPosition) {
         newVal = curVal + 1;
-    } else {
-        newVal = 1;
-    };
+    }
     this.style.gridColumnStart = newVal;
+    environmentVar[btnName] = newVal;
 }
 
-const powerToggle = Array.from(document.getElementsByClassName('slide__toggle'));
-powerToggle.forEach(toggle => toggle.addEventListener('click', toggleButton));
+buttonToggle.forEach(toggle => toggle.addEventListener('click', toggleButton));
+
+function isOn() {
+    if (environmentVar.power === 1) return 1; // power === 1 is off, 2 is on
+}
 
 function startGame() {
+    if (environmentVar.power === 1) return; // power === 1 is off, 2 is on
+
     computerTurn();
 }
+
 
 document.getElementsByClassName("js-start")[0].addEventListener('click', startGame);
 
@@ -36,11 +53,6 @@ document.getElementsByClassName("js-start")[0].addEventListener('click', startGa
 function playSound() {
     var key = this.dataset.key;
     playButton(key);
-    // if (playerTurn) {
-    //     player.push(key);
-    //     console.log("Player: ");
-    //     console.log(player);
-    // }
 }
 
 const buttons = Array.from(document.querySelectorAll('.game-button'));
@@ -54,12 +66,23 @@ var computerSequence = [];                                  // Computer sequence
 var playerSequence = 0;                                     // Array to register the player
 var playerTurn = false;
 
+var skillLevel = {
+    1: 8,
+    2: 14,
+    3: 20,
+    4: 31,
+    5: 42,
+}
+
+
 function computerTurn() {
     const max = 3;
     var random = Math.floor(Math.random() * max + 1);
     var newKey = buttonsKey[random];
     console.log("computerTurn");
     
+    
+
     for (let i = 0; i <= computerSequence.length; i++) {
         (function(val) {
             setTimeout(() => {
