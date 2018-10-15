c41d87acdfac0ba1b0400d0c8081a5f34a4dc28a
Fabien SHAN <shan.fabien@gmail.com>
2018-10-15 16:38:48 +1300

    Changed Schema of app state (added name and url of sound). Set state to get the name of the sound to display

--- new_curriculum_projects/drum_machine/src/App.js
+++ new_curriculum_projects/drum_machine/src/App.js
@@ -12,7 +12,10 @@ class Drumboard extends Component {
           key={key}
           onClick={(e) => this.props.playSound(e)}>
 
-          <audio data-key={keyCode} src={this.props.soundKit[i]}></audio>
+          <audio
+            data-key={keyCode}
+            data-name={this.props.soundKit[i].name}
+            src={this.props.soundKit[i].url}></audio>
           {key}
         </div>
       )
@@ -33,27 +36,82 @@ class App extends Component {
     padKeys: ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'],
     soundKits: {
       HeaterKit: [
-        "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-      ],
+        {
+          name: "Heater 1",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+        },
+        {
+          name: "Heater 2",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+        },
+        {
+          name: "Heater 3",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+        },
+        {
+          name: "Heater 4",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+        },
+        {
+          name: "Clap",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+        },
+        {
+          name: "Open HH",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+        },
+        {
+          name: "Kick n'Hat",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+        },
+        {
+          name: "Kick",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+        },
+        {
+          name: "Closed HH",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.m"
+        }
+      ]
+      ,
       SmoothPianoKit: [
-        "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
-        "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
-      ],
+        {
+          name: "Chord 1",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+        },
+        {
+          name: "Chord 2",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+        },
+        {
+          name: "Chord 3",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+        },
+        {
+          name: "Shaker",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+        },
+        {
+          name: "Open HH",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+        },
+        {
+          name: "Closed HH",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+        },
+        {
+          name: "Punchy Kick",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+        },
+        {
+          name: "Side Stick",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+        },
+        {
+          name: "Snare",
+          url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+        },
+      ]
     }
   }
 
@@ -67,6 +125,10 @@ class App extends Component {
     audio.volume = this.state.volume;
     audio.play();
     key.classList.add('playing');
+
+    this.setState({
+      display: audio.dataset.name,
+    });
   }
 
   removeTransition = (e) => {
